c23b8571a2eb706b77f7f04e9bd91ae204169413
kallada2021 <karthikeyaallada77@gmail.com>
2022-01-28 23:44:51 -0600

    try

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0a8b4cd432b1c3063"
-  instance_type = "t2.nano"
+  ami           = "ami-01893222c83843146"
+  instance_type = "t2.micro"
 }
